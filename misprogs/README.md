479bd6d825719ce32a44ecb07612ec19aff5ee8b
rsolisuaz <rsolis@uaz.edu.mx>
2017-06-21 14:26:11 +0000

    Nuevo directorio con programas en Python
    Aqui iria una descripcion mas detalllada
    de lo que estamos haciendo commit
    
    Signed-off-by: rsolisuaz <rsolis@uaz.edu.mx>

---
+++ misprogs/README.md
@@ -0,0 +1,3 @@
+Mis programas Python para el curso de Intel
+Galileo en la H. UAZ
+
---
+++ misprogs/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import time
+while True:
+   print "Hola desde el curso de Intel"
+   time.sleep(5)
+
+#Fin de archivo
